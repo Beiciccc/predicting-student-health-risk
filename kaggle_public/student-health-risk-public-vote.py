fdee177831e7c23dc2ba3f2c47fd461a6cb03a68
Kun Zhang <kun027901@gmail.com>
2026-07-13 01:39:00 +0100

    Record submission s279

--- kaggle_public/student-health-risk-public-vote.py
+++ kaggle_public/student-health-risk-public-vote.py
@@ -133,8 +133,8 @@ OVERRIDES = {
     697096: "at-risk",
     768924: "fit",
     776133: "at-risk",
-    965287: "fit",
-    977425: "unhealthy",
+    965287: "at-risk",
+    977425: "at-risk",
     982267: "fit",
 }
 
